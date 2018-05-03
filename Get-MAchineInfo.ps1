cb7ebda8c582a81701ca8809a255bbb4ee2e8a2f
Kamil Procyszyn <kprocyszyn@gmail.com>
2018-05-03 20:50:28 +0100

    15

--- Get-MAchineInfo.ps1
+++ Get-MAchineInfo.ps1
@@ -25,56 +25,105 @@ Function Get-MachineInfo {
             ELSE {
                 $option = New-CimSessionOption -Protocol Wsman
             }
-            #Connect session
-            Write-Verbose "Connecting to $Computer over $protocol"
-            $session = New-CimSession -ComputerName $computer -SessionOption $option
-    
-            # Query data
-            Write-Verbose "Querying from $Computer"
-            $os_params = @{'ClassName' = 'Win32_OperatingSystem'
-                'CimSession' = $Session
-            }
-            $os = Get-CimInstance @os_params
-
-            $cs_params = @{'ClassName' = 'Win32_ComputerSystem'
-                'CimSession' = $session
-            }
-            $cs = Get-CimInstance @cs_params
-
-            $sysdrive = $os.SystemDrive
-            $drive_params = @{'ClassName' = 'Win32_LogicalDisk'
-                'Filter' = "DeviceId='$sysdrive'"
-                'CimSession' = $session
-            }
-            $drive = Get-CimInstance @drive_params
-
-            $proc_params = @{'ClassName' = 'Win32_Processor'
-                'CimSession' = $session
-            }
-            $proc = Get-CimInstance @proc_params |
-                Select-Object -First 1
-
-            #CLose session
-            Write-Verbose "Closing session to $Computer"
-            $session | Remove-CimSession
-
-            #Output data
-            Write-Verbose "Outputing from $computer"
-            $obj = [pscustomobject]@{
-                'ComputerName' = $computer
-                'OSVersion' = $os.Version
-                'SPVersion' = $os.ServicePackMajorVersion
-                'OSBuild' = $os.OSBuild
-                'Manufacturer' = $cs.Manufacturer
-                'Model' = $cs.Model
-                'Procs' = $cs.numberofprocessors
-                'Cores' = $cs.numberoflogicalprocessors
-                'RAM' = ($cs.totalphysicalmemory / 1GB)
-                'Arch' = $proc.addresswidth
-                'SysdriveFreeSpace' = $drive.FreeSpace
-            }
             
-            Write-Output $obj
+            Try {
+                #Connect session
+                Write-Verbose "Connecting to $Computer over $protocol"
+                $params = @{
+                    'ComputerName' = $computer
+                    'SessionOption' = $option
+                    'ErrorAction' = 'Stop'
+                }
+                $session = New-CimSession @params
+    
+                # Query data
+                Write-Verbose "Querying from $Computer"
+                $os_params = @{'ClassName' = 'Win32_OperatingSystem'
+                    'CimSession' = $Session
+                }
+                $os = Get-CimInstance @os_params
+
+                $cs_params = @{'ClassName' = 'Win32_ComputerSystem'
+                    'CimSession' = $session
+                }
+                $cs = Get-CimInstance @cs_params
+
+                $sysdrive = $os.SystemDrive
+                $drive_params = @{'ClassName' = 'Win32_LogicalDisk'
+                    'Filter' = "DeviceId='$sysdrive'"
+                    'CimSession' = $session
+                }
+                $drive = Get-CimInstance @drive_params
+
+                $proc_params = @{'ClassName' = 'Win32_Processor'
+                    'CimSession' = $session
+                }
+                $proc = Get-CimInstance @proc_params |
+                    Select-Object -First 1
+
+                #CLose session
+                Write-Verbose "Closing session to $Computer"
+                $session | Remove-CimSession
+
+                #Output data
+                Write-Verbose "Outputing from $computer"
+                $obj = [pscustomobject]@{
+                    'ComputerName' = $computer
+                    'OSVersion' = $os.Version
+                    'SPVersion' = $os.ServicePackMajorVersion
+                    'OSBuild' = $os.OSBuild
+                    'Manufacturer' = $cs.Manufacturer
+                    'Model' = $cs.Model
+                    'Procs' = $cs.numberofprocessors
+                    'Cores' = $cs.numberoflogicalprocessors
+                    'RAM' = ($cs.totalphysicalmemory / 1GB)
+                    'Arch' = $proc.addresswidth
+                    'SysdriveFreeSpace' = $drive.FreeSpace
+                }
+            
+                Write-Output $obj
+            }
+            Catch {
+                Write-Warning -Message "FAILED $computer on $protocol"
+
+                #Did we specify protocol fallback?
+                #If so, try again. If we specified logging,
+                #we won't log a problem here - we'll let
+                #the logging occur if this fallback also
+                #fails
+                If ($Protocol -eq 'Dcom') {
+                    $newprotocol = 'Wsman'
+                }
+                ELSE {
+                    $newprotocol = 'Dcom'
+                } #if protocol
+
+                Write-Verbose -Message "Trying again with $newprocol"
+                $params = @{
+                    'ComputerName' = $computer
+                    'Protocol' = $newprotocol
+                    'ProtocolFallback' = $false
+                }
+
+                If ($PSBoundParameters.ContainsKey('LogFailurePath')) {
+                    $params += @{'LogFailurePath' = $LogFauilurePath}
+                } #if logging
+                Get-MachineInfo @params
+
+            } #if protocolfallback
+
+            #if we didn't specify fallback,
+            #but we did specify logging, then log the error
+            #because we won't be trying again
+
+            if (-not $ProtocolFallback -and
+                $PSBoundParameters.ContainsKey('LogFailurePath')) {
+                Write-Verbose "Logging to $LogFailurePath"
+                $computer | Out-File $LogFauilurePath -Append
+            } #if write to log
+
+            #} #try/catch
+        
         } #foreach
     } #Process
     END {}
--- Set-TMServiceLogon.ps1
+++ Set-TMServiceLogon.ps1
@@ -1,4 +1,33 @@
 function Set-TMServiceLogon {
+    <#
+   .SYNOPSIS
+   Tool to change username and password of given service.
+   
+   .DESCRIPTION
+    Use this tool to change one service on multiple computers. It uses WSMAN therefore must be opened on firewall.
+
+    .PARAMETER ServiceName
+    Name of the service required. Mandatory.
+
+    .PARAMETER NewPassword
+    New password for the service. Mandatory.
+
+    .PARAMETER NewUser
+    New user under which the service will run
+
+    .PARAMETER ComputerName
+    Name of computer(s)
+
+    .PARAMETER EorrLogFilePath
+    Path to the error file
+
+    .EXAMPLE
+    Set-TMServiceLogon -ServiceName 'BITS' -ComputerName file1 -NewUsername tm.local\admin1 -NewPassword SecretOne
+    This function will change the service "BITS" on Computer "File1", running it as "tm.local\admin1" with Password "SecretOne"
+
+   #>
+   
+   
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
@@ -21,50 +50,69 @@ function Set-TMServiceLogon {
     Process {
 
         foreach ($computer in $ComputerName) {
+            Do {
+                Write-Verbose "Connecting to $Computer on WS-MAN"
+                $Protocol = 'Wsman'
             
-           
-            $option = New-CimSessionOption -Protocol Wsman
-            #Connect session
-            Write-Verbose "Connecting to $ComputerName on WS-MAN"
-            $Session = New-CimSession -SessionOption $option -ComputerName $COmputerName
+                Try {
+                    $option = New-CimSessionOption -Protocol $Protocol
+                    #Connect session
+            
+                    $Session = New-CimSession -SessionOption $option -ComputerName $Computer -ErrorAction Stop
 
-            If ($PSBoundParameters.ContainsKey('NewUser')) {
-                $args = @{
-                    'StartName' = $NewUser
-                    'StartPassword' = $NewPassword
+                    If ($PSBoundParameters.ContainsKey('NewUser')) {
+                        $args = @{
+                            'StartName'     = $NewUser
+                            'StartPassword' = $NewPassword
+                        }
+                    }
+                    ELSE {
+                        $args = @{'StartPassword' = $NewPassword}
+                        Write-Warning "Not setting a new user name"
+                    }
+                    #Change service
+                    Write-Verbose "Setting $servicename on $computer"
+                    $params = @{
+                        'Query'        = "SELECT * FROM Win32_Service WHERE Name='$ServiceName'"
+                        'MethodName'   = 'Change'
+                        'ComputerName' = $computer
+                        'Arguments'    = $args
+                    }
+                    $ret = Invoke-CimMethod @params
+           
+                    Switch ($ret.ReturnValue) {
+                        "0" {$Status = "Success"}
+                        "22" {$Status = 'Wrong user account'}
+                        default {$Status = "Failed: $($ret.ReturnValue)"}
+                    }
+            
+                    $props = @{
+                        'ComputerName' = $computer
+                        'Status'       = $Status
+                    }
+                    $obj = New-Object -TypeName psobject -Property $props
+                    Write-Output $obj
+
+                    #Close session
+                    Write-Verbose "Clossing session"
+                    $Session | Remove-CimSession
+            
                 }
-            }
-            ELSE {
-                $args = @{'StartPassword' = $NewPassword}
-            }
-            #Change service
-            Write-Verbose "Setting $servicename on $computer"
-            $params = @{
-                'Query' = "SELECT * FROM Win32_Service WHERE Name='$ServiceName'"
-                'MethodName' = 'Change'
-                'ComputerName' = $computer
-                'Arguments' = $args
-            }
-            $ret = Invoke-CimMethod @params
-           
-            Switch ($ret.ReturnValue) {
-                "0" {$Status = "Success"}
-                "22" {$Status = 'Wrong user account'}
-                default {$Status = "Failed: $($ret.ReturnValue)"}
-            }
-           
-            #Close session
-            Write-Verbose "Clossing session"
-            $Session | Remove-CimSession
-            #Output data
-            Write-Verbose "Outputing data" 
-            $props = @{
-                'ComputerName' = $computer
-                'Statis' = $Status
-            }
-            New-Object -TypeName psobject -Property $props
-            Write-Output $props
+                Catch {
+                    # Change protocol - if we've tried both and logging was specified, log the computer
+                    Switch ($Protocol) {
+                        'Wsman' {$Protocol = 'Dcom'}
+                        'Dcom' {
+                            $Protocol = 'Stop' 
+                            if ($PSBoundParameters.ContainsKey('ErrorLogFilePath')) {
+                                Write-Warning "$computer failed: logged to $errorlogfilepath"
+                                $computer | Out-File $ErrorLogFilePath -Append
+                            } #if logging
+                        }
+                    } #switch
 
+                }# try/catch
+            } until ($Protocol -eq 'Stop')
         }#foreach
     }#process
 
