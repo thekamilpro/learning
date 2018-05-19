b8f2bac2bf1640bc397079fca87ad5615985238a
Kamil Procyszyn <kprocyszyn@gmail.com>
2018-05-19 15:38:22 +0100

    1

--- DSCWorkshop/Task 1.ps1
+++ DSCWorkshop/Task 1.ps1
@@ -1,36 +1,44 @@
 Configuration DSC101Simple {
 
+param (
+    [Parameter()]
+    [Boolean] $ICMP = $false,
+
+    [Parameter(Mandatory=$true)]
+    [Switch] $DisableServerManager
+)
+
     Import-DscResource -ModuleName xNetworking
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName PolicyFileEditor
 
     xFirewall AllowPingIPv4 {
-        'Name' = 'FPS-ICMP4-ERQ-In' ;
-        'Direction' = 'Inbound' ;
-        'Action' = 'Allow' ;
-        'Profile' = 'Any' ;
-        'Enabled' = 'True' ;
+        'Name' = 'FPS-ICMP4-ERQ-In'
+        'Direction' = 'Inbound'
+        'Action' = 'Allow'
+        'Profile' = 'Any'
+        'Enabled' = $ICMP.ToString()
     } #    xFirewall AllowPingIPv4 
 
     xFirewall AllowPingIpv6 {
-        'Name' = 'FPS-ICMP-ERQ-In' ;
-        'Direction' = 'Inbound' ;
-        'Action' = 'Allow' ;
-        'Profile' = 'Any' ;
-        'Enabled' = 'True' ;
+        'Name' = 'FPS-ICMP6-ERQ-In'
+        'Direction' = 'Inbound'
+        'Action' = 'Allow'
+        'Profile' = 'Any'
+        'Enabled' = $ICMP.ToString()
     } #xFirewall AllowPingIpv6
 
     Registry BlockServerManagerAutostart {
-        'Key' = 'HKLM:\\Software\Microsoft\ServerManager\'
+        'Key' = 'HKLM:\Software\Microsoft\ServerManager\'
         'ValueName' = 'DoNotOpenServerManagerAtLogon' 
         'ValueType' = 'Dword'
-        'ValueData' = '1'
+        'ValueData' =  $DisableServerManager.ToBool() -as [System.Int32]
     } # Registry BlockServerManagerAutostart
 
     cAdministrativeTemplateSetting EnablePsTranscription {
-
+            #Applies after gpupdate
         'PolicyType' = 'Machine'
-        'KeyValueName' = 'HKLM:\Software\Policies\Microsoft\Windows\Powershell\Transcription\EnableTranscripting'
+        'KeyValueName' = 'Software\Policies\Microsoft\Windows\Powershell\Transcription\EnableTranscripting'
         'Type' = 'Dword'
         'Data' = '1'
         'Ensure' = 'Present'
@@ -39,6 +47,6 @@ Configuration DSC101Simple {
 } #configuration
 
 
-DSC101Simple -OutputPath C:\DSC  #generates the MOF file
+DSC101Simple -OutputPath C:\DSC  -DisableServerManager $true -ICMP $true #generates the MOF file
 
 Start-DscConfiguration -Path c:\DSC -Verbose -Wait #run the configuration
