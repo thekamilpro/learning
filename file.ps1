776b787a0faffcd4019e441d9c043977d75885fe
Kamil Procyszyn <kprocyszyn@gmail.com>
2018-06-11 21:15:11 +0100

    cim

---
+++ file.ps1
@@ -0,0 +1,23 @@
+$Cimparams = @{Computername = $Computername}
+$props = @{'PSComputerName'}
+
+Switch ($Value) {
+    'OS' {
+        $Cimparams.Add('classname','win32_operatingsystem')
+        $Props += 'Version','Caption'
+    }
+    'CS' {
+        $Cimparams.Add('classname','win32_operatingsystem')
+        $Props += 'Model','Manufacturer'
+    }
+    'CPU' {
+        $Cimparams.Add('Classname','win32_Processor')
+        $Props += 'CPUID','Name','MacClockSpeed'
+    }
+    'Memory' {
+        $Cimparams.Add('Classname','Win32_physicalMemory')
+        $props += 'Banklabel','Capacity','Speed'
+    }
+}
+
+$Data = Get-CimInstance @Cimparams
