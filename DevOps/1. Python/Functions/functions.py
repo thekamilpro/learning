4183d840e1b294dc4721d39d93f2a01b341a6748
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-09-26 07:00:30 +0100

    fix bug to use name from the list

--- DevOps/1. Python/Functions/functions.py
+++ DevOps/1. Python/Functions/functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
 def print_students_titlecase():
@@ -13,13 +13,12 @@ def print_students_titlecase():
 def add_student(name, student_id=332):
     student = {"name": name, "student_id": student_id}
     students.append(student)
-
-def var_args (name, *args): #parameters *args takes multiple variables
-    print (name)
-    print (args)
-
+    
 student_list = get_students_titlecase()
 
-add_student(name="Mark", student_id=15)
+#Read-Host
+student_name = input("Enter student name: ")
+student_id = input("Enter student ID: ")
 
-var_args("Mark, Loves Python", None, "Hello", True)
+add_student(student_name, student_id)
+print_students_titlecase()
