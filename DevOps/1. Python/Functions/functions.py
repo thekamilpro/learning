7f3351a0137a25fa91c7c8de584dbb7033b5520b
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-09-26 06:42:56 +0100

    Added function

--- DevOps/1. Python/Functions/functions.py
+++ DevOps/1. Python/Functions/functions.py
@@ -7,10 +7,12 @@ def get_students_titlecase():
     return students_titlecase
 
 def print_students_titlecase():
-    students_titlecase = []
-    for student in students:
-        students_titlecase = student.title()
+    students_titlecase = get_students_titlecase()
     print(students_titlecase)
 
 def add_student(name):
     students.append(name)
+
+student_list = get_students_titlecase()
+
+add_student("Mark")
