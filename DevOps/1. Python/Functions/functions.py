164ea216292d7631f039c7df04bab069b741b3c2
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-09-27 08:43:43 +0100

    Function now shows the content of file

--- DevOps/1. Python/Functions/functions.py
+++ DevOps/1. Python/Functions/functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
