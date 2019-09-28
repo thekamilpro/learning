574a3559e02d6822afd535c157fa5e0c46807a13
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-09-28 16:35:52 +0100

    basic class

---
+++ DevOps/1. Python/Classes/classes.py
@@ -0,0 +1,10 @@
+class Student:
+    pass #pass is a keyword in Python meaning: Do nothing.
+
+student = Student() #defining new instance of Student class.
+
+print(student)
+
+new_student = Student()
+
+print(new_student)
