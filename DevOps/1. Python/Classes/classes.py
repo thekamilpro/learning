4f301e1916fed9ec5748620ea36d2799271f3d3b
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-09-28 16:51:32 +0100

    instance and attribute

--- DevOps/1. Python/Classes/classes.py
+++ DevOps/1. Python/Classes/classes.py
@@ -1,12 +1,24 @@
 students = []
 
 class Student:
+
+    school_name = "Springfield Elementary"
+
     def __init__(self, name, student_id=332):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
 
-mark = Student("Mark")
-print(mark)
+    def get_name_capitalize(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
+
+# mark = Student("Mark")
+# print(mark)
+
+print(Student.school_name)
