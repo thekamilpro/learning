1cdc4a6bbb867b1a4034d328fcf21f0b7307fdb1
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-11-21 13:46:51 +0000

    foo

--- Python/Modularity/words.py
+++ Python/Modularity/words.py
@@ -1,15 +1,22 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+    return story_words            
 
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
 
 if __name__ == '__main__':
-    fetch_words()
+   main(sys.argv[1])
