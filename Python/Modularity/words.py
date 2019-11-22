f0cc5bf0b9622bd22350291261a75d0f4338e4a5
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-11-22 06:57:03 +0000

    Comments to the module and functions

--- Python/Modularity/words.py
+++ Python/Modularity/words.py
@@ -1,7 +1,23 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+    
+    python words.py <URL
+"""
+
 import sys
 from urllib.request import urlopen
 
 def fetch_words(url):
+    """"Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -11,10 +27,20 @@ def fetch_words(url):
     return story_words            
 
 def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 def main(url):
+    """Print each word from a text document from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
