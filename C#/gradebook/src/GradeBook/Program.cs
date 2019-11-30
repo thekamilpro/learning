8f1d127d78f8319446c5ea7589c54776a1077a97
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-11-30 15:56:38 +0000

    Initial commit

---
+++ C#/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var grades = new List<double>() {34.1, 35, 1.2};
+            grades.Add(56);
+        
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+             result /=  grades.Count;
+
+            System.Console.WriteLine($"The average grade is: {result}");
+
+            if(args.Length > 0)
+            {
+            Console.WriteLine($"Hello, {args[0]} !");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
