47ce261704349fbfd50175a5a0389842e80a39cc
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-07 14:55:56 +0000

    Added grade validation

--- C#/gradebook/src/GradeBook/Book.cs
+++ C#/gradebook/src/GradeBook/Book.cs
@@ -14,7 +14,15 @@ namespace GradeBook
 
         public void AddGrade(double grade)
         {
+
+            if ( grade <= 100 && grade >= 0)
+            {
             grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
         }
 
         public Statistics GetStatistics()
