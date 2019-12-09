8a180d4ac5f8d1aabf3f70e9fbd208bdd9066b8c
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-09 13:21:36 +0000

    Added more powerful switch

--- C#/gradebook/src/GradeBook/Book.cs
+++ C#/gradebook/src/GradeBook/Book.cs
@@ -65,6 +65,32 @@ namespace GradeBook
             }
             result.Average /= grades.Count;
 
+            switch(result.Average)
+            {
+                case var d when d>= 90.0: //var d comes from .Average, and will only be used if when return true.
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
+
             return result;
 
         }
--- C#/gradebook/src/GradeBook/Program.cs
+++ C#/gradebook/src/GradeBook/Program.cs
@@ -21,6 +21,7 @@ namespace GradeBook
             System.Console.WriteLine($"The average grade is: {stats.Average:N1}");
             System.Console.WriteLine($"The maximum grade is: {stats.High}");
             System.Console.WriteLine($"The smallest grade is: {stats.Low}");
+            System.Console.WriteLine($"The letter grade is: {stats.Letter}");
 
         }
     }
