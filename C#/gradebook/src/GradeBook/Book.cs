397545b5267066472821219d6e8fe72cb7003e81
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-15 15:00:32 +0000

    Refactored statistics class

--- C#/gradebook/src/GradeBook/Book.cs
+++ C#/gradebook/src/GradeBook/Book.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace GradeBook
 {
@@ -19,15 +20,61 @@ namespace GradeBook
         }
     }
 
-    public abstract class Book : NamedObject
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name {get;}
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : NamedObject, IBook
     {
         public Book(string name) : base(name)
         {
         }
 
+        public abstract event GradeAddedDelegate GradeAdded;
         public abstract void AddGrade(double grade);
+        public abstract Statistics GetStatistics();
+
+    }
+
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
         {
-            // ..
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            
+            using(var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
         }
     }
     public class InMemoryBook : Book
@@ -38,30 +85,7 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
-
-        public override void AddGrade(char letter)
-        {
-            switch(letter)
-            {
-                case 'A':
-                    AddGrade(90);
-                    break;
-
-                case 'B':
-                    AddGrade(80);
-                    break;
-
-                case 'C':
-                    AddGrade(70);
-                    break;
-
-                default:
-                    AddGrade(0);
-                    break;
-            }
-        }
-
-        public void AddGrade(double grade)
+        public override void AddGrade(double grade)
         {
 
             if ( grade <= 100 && grade >= 0)
@@ -78,63 +102,23 @@ namespace GradeBook
             }
         }
 
-        public event GradeAddedDelegate GradeAdded;
+        public override event GradeAddedDelegate GradeAdded;
 
-        public Statistics GetStatistics()
+        public override Statistics GetStatistics()
         {
 
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
-
             
             for (var index = 0; index < grades.Count; index =+ 1)
             {
-                result.Low = Math.Min(grades[index], result.Low);
-                result.High = Math.Max(grades[index], result.High);
-                result.Average += grades[index];
+                result.Add(grades[index]);
                 
             }
-            result.Average /= grades.Count;
-
-            switch(result.Average)
-            {
-                case var d when d>= 90.0: //var d comes from .Average, and will only be used if when return true.
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-
-
+          
             return result;
-
         }
 
         private List<double> grades;
-
-        public string Name
-        {
-          get; 
-          set;
-        }
 
         readonly string category = "Science";
     }
