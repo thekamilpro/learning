aec8a937331404bbc650fc23df12f3497d1dcd0a
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-22 11:13:01 +0000

    Initial commit

---
+++ C#/ACM/ACM.BL/OrderItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+
+        public int OrderItemId { get; private set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (Quantity <= 0) isValid = false;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasePrice == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ C#/ACM/Tests/ACM.BL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BL.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BL.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57cebc78-8821-46cc-a4f4-220cd46fa820")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
