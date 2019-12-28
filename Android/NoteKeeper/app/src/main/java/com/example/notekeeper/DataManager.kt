175033815b7e91a849fd19596a88211d422692f6
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-28 17:17:23 +0000

    Added couple of classes

---
+++ Android/NoteKeeper/app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -0,0 +1,25 @@
+package com.example.notekeeper
+
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo( "android_intents","Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform" )
+        courses.set(course.courseId, course)
+    }
+
+}
---
+++ Android/NoteKeeper/app/src/main/java/com/example/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,5 @@
+package com.example.notekeeper
+
+class CourseInfo (val courseId: String, val title: String)
+
+class NoteInfo (var course: CourseInfo, var title: String, var text: String)
