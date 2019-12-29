69d8c209399cc78cb5002e466d8ee31caef40e0c
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-29 17:39:21 +0000

    Added spinner and populated with data

--- Android/NoteKeeper/app/src/main/java/com/example/notekeeper/NoteKeeperData.kt
+++ Android/NoteKeeper/app/src/main/java/com/example/notekeeper/NoteKeeperData.kt
@@ -1,5 +1,9 @@
 package com.example.notekeeper
 
-class CourseInfo (val courseId: String, val title: String)
+class CourseInfo (val courseId: String, val title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
 
 class NoteInfo (var course: CourseInfo, var title: String, var text: String)
