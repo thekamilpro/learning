4b00b82598de5b1c70fd43e0ccf53eed0754da3b
Kamil Procyszyn <kprocyszyn@gmail.com>
2019-12-30 19:23:13 +0000

    Added another view and intent

---
+++ Android/NoteKeeper/app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -0,0 +1,23 @@
+package com.example.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+           val activityIntent = Intent( this , MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+    }
+
+}
